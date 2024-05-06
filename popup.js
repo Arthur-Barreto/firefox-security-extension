3e2e54123ae236d599b2582b0e0c49def6c7c789
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-05-06 20:48:09 -0300

    ajuste final conexoes externas

--- popup.js
+++ popup.js
@@ -2,23 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
     const countDiv = document.getElementById('count');
     const toggleButton = document.getElementById('toggleButton');
     const dropdown = document.getElementById('dropdown');
-    const results = document.getElementById('results');
+    const resultsElement = document.getElementById('results');
 
-    // Solicita as conexões ao abrir o popup
-    browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-        browser.runtime.sendMessage({ action: "get_connections", tabId: tabs[0].id }, function (response) {
-            const connections = response.connections;
-            if (connections && connections.length > 0) {
-                countDiv.textContent = `Conexões de Terceiros: ${connections.length}`;
-                connections.forEach(url => {
-                    let li = document.createElement('li');
-                    li.textContent = url;
-                    results.appendChild(li);
-                });
-                toggleButton.style.display = 'block';
-                toggleButton.textContent = 'Mostrar Conexões';
-            } else {
-                countDiv.textContent = "Nenhuma conexão de terceira parte detectada.";
+    // Obtém o ID da aba atual
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tabId = tabs[0].id;  // Assume que sempre haverá pelo menos uma aba
+
+        browser.runtime.sendMessage({ action: "get_connections", tabId: tabId }, function (response) {
+            resultsElement.innerHTML = ''; // Limpa os resultados antigos
+            if (response && response.connections) {
+                let connections = response.connections;
+                if (Object.keys(connections).length > 0) {
+                    Object.keys(connections).forEach(url => {
+                        const count = connections[url];
+                        let li = document.createElement('li');
+                        li.textContent = `${url} (acessado ${count} vezes)`;
+                        resultsElement.appendChild(li);
+                    });
+                    toggleButton.style.display = 'block'; // Mostra o botão se existirem conexões
+                    toggleButton.textContent = 'Mostrar Conexões'; // Define o texto inicial do botão
+                    countDiv.textContent = `Conexões Externas: ${Object.keys(connections).length}`;
+                } else {
+                    countDiv.textContent = "Nenhuma conexão de terceira parte detectada.";
+                    toggleButton.style.display = 'none'; // Esconde o botão se não existirem conexões
+                }
             }
         });
     });
